7fa4b8957d42a0c78d51d94952a4ac859124f44c
Jonathan Lee <jonathanlee882@gmail.com>
2023-06-28 23:46:17 +1000

    added error functionality

--- part3.js
+++ part3.js
@@ -21,7 +21,7 @@ function renderTasks() {
         return (
         `<li class="task">
           ${task.text}
-          <button class="todo__delete">
+          <button class="todo__delete" onclick=removeTask(${task.id})>
             x
           </button>
         </li>`
@@ -31,8 +31,57 @@ function renderTasks() {
 
 renderTasks()
 
-let htmlTasks = document.querySelectorAll(".task") 
-console.log(htmlTasks)
+let $inputEl = document.querySelector("input")
+
+let errorText = document.querySelector("#errorText")
+
+function addTask (newTask) {
+    if (!newTask.length) {
+        errorText.style.display = "block"
+        return
+    }
+    errorText.style.display = "none"
+    // tasks.push({
+    //     id: tasks.length + 1,
+    //     text: newTask
+    // })
+    // renderTasks()
+    // $inputEl.value = ""
+
+    let $newTaskEl = document.createElement("li")
+    htmlList.appendChild($newTaskEl)
+
+    let $taskSpan = document.createElement("span")
+    $taskSpan.innerText = newTask;
+    $newTaskEl.appendChild($taskSpan)
+
+    let $buttonEl = document.createElement("button")
+    $buttonEl.innerText = "delete"
+    $newTaskEl.appendChild($buttonEl)
+
+}
+
+
+let addButton = document.querySelector("button")
+addButton.addEventListener("click", (onchange) => addTask($inputEl.value))
+
+function removeTask (id) {
+    console.log(`before our tasklist was ${tasks.length} long`)
+    tasks = tasks.filter((task) => {
+        if (task.id !== id) {
+            console.log("we return")
+            return task
+        } else {
+            console.log("no match")
+        }
+    })
+    console.log(`after our tasklist is ${tasks.length} long`)
+    renderTasks()
+}
+
+// addButton.addEventListener("click", () => {
+//     addTask($inputEl.value)
+// })
 
 // htmlTasks.forEach((task) => {
 //     addEventListener("click", (task) => {
